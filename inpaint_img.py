32600cfd481f8a3054c00c1351165dd7ae3496cc
StellarApp <testcodesk@gmail.com>
2021-07-08 21:54:28 -0500

    inpaint img

--- inpaint_img.py
+++ inpaint_img.py
@@ -1,33 +1,52 @@
-import cv2
-from keras.models import model_from_json
-import tensorflow as tf
-import keras
-import tensorflow as tf
-from tensorflow import keras
-from keras.models import load_model
-from keras.preprocessing import image
-from pathlib import Path
-# from pconv_layer import PConv2D
 
-fileName = "starbucks_01.jpg"
+from keras.models import load_model
+from pconv_layer import PConv2Ding
+
+fileName = "Starbucks_01.jpg"
 filePath = f"{fileName}"
-maskPath = f"mask_{fileName}"
+maskPath = f"Mask_{fileName}"
 
 # Reading an image in default mode
-img = cv2.imread(filePath)
-mask = cv2.imread(maskPath)
+# img = cv2.imread(filePath)
+# mask = cv2.imread(maskPath)
 
-# Load the json file that contains the model's structure
-f = Path("model.json")
-model_structure = f.read_text()
+img = image.load_img(filePath, target_size=(32, 32, 3))
+mask = image.load_img(maskPath, target_size=(32, 32, 3))
 
-model = model_from_json(model_structure)
+# data = [[],[]]
+#
+# data[0].append(asarray(img))
+# data[1].append(asarray(mask))
+#
+# print(len(data))
+# print(len(data[0][0]))
+# print(len(data[1][0]))
+#
+# np.save('data', np.array(data))
 
-# model = load_model('/data/Auditory_Emotion_Recognition/model_attention/fianl_emotion_model.h5',custom_objects={'SeqSelfAttention':SeqSelfAttention})
-model = load_model(model)
-model.load_weights("model_weights.h5")
+# f = open("data.txt", "a")
+# f.write(data.__str__())
+# f.close()
 
-image_to_test = image.image_to_array(img)
+# f = open("data.txt", "r")
+# data = f.read()
+# print(len(data[0][0]))
 
-inputs = [img.reshape((1,)+img.shape), mask.reshape((1,)+mask.shape)]
-model.prdict(inputs)
+
+# # Load the json file that contains the model's structure
+# f = Path("model.json")
+# model_structure = f.read_text()
+#
+# model = model_from_json(model_structure)
+#
+model = load_model('model/model_ipc', custom_objects={'PConv2D': PConv2D})
+# model = load_model(model)
+# model.load_weights("model_weights.h5")
+
+print("test1")
+print(model.get_config())
+print("test2")
+# image_to_test = image.image_to_array(img)
+#
+# inputs = [img.reshape((1,)+img.shape), mask.reshape((1,)+mask.shape)]
+# model.prdict(inputs)
